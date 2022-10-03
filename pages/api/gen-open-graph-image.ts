4e0814e5edfb8a2ea0850480d1b1d1c0d0cdcd13
Emmahchinonso <emmanuelchinons9@gmail.com>
2022-10-03 14:53:47 +0100

    use playwright

--- pages/api/gen-open-graph-image.ts
+++ pages/api/gen-open-graph-image.ts
@@ -1,5 +1,6 @@
-import puppeteer from "puppeteer-core";
-import chrome from "chrome-aws-lambda";
+// import puppeteer from "puppeteer-core";
+// import chrome from "chrome-aws-lambda";
+import playwright from "playwright-aws-lambda";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = Buffer | string;
@@ -8,23 +9,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const options = process.env.AWS_REGION
-    ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      }
-    : {
-        args: [],
-        executablePath:
-          process.platform === "win32"
-            ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-            : process.platform === "linux"
-            ? "/usr/bin/google-chrome"
-            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-      };
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
+  // const options = process.env.AWS_REGION
+  //   ? {
+  //       args: chrome.args,
+  //       executablePath: await chrome.executablePath,
+  //       headless: chrome.headless,
+  //     }
+  //   : {
+  //       args: [],
+  //       executablePath:
+  //         process.platform === "win32"
+  //           ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+  //           : process.platform === "linux"
+  //           ? "/usr/bin/google-chrome"
+  //           : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  //     };
+  const browser = await playwright.launchChromium();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
   const html = `
     <!DOCTYPE html>
@@ -42,14 +44,21 @@ export default async function handler(
 
   await page.setContent(html);
 
-  const rootContainer = await page.$("#root");
-  const boundingRect = await rootContainer?.boundingBox();
+  // const rootContainer = await page.$("#root");
+  // const boundingRect = await rootContainer?.boundingBox();
+
+  const boundingRect = await page.evaluate(() => {
+    const rootElement = document.getElementById("root");
+    const { x, y, width, height } =
+      rootElement?.children[0].getBoundingClientRect()!;
+    return { x, y, width, height };
+  });
 
   const screenShotBuffer = await page.screenshot({ clip: boundingRect! });
 
   res.setHeader("Content-Type", "image/png");
   res.setHeader("Content-Length", screenShotBuffer.length.toString());
 
-  res.status(200).send(screenShotBuffer);
+  res.status(200).send(screenShotBuffer.toString("base64"));
   await browser.close();
 }
